8af625a245e48b704dc29568c3f0603d874e2bce
unknown <jefersonmallone2000@outlook.com>
2021-08-24 20:23:41 -0300

    no message

--- App.js
+++ App.js
@@ -3,10 +3,14 @@ import Home from './src/screens/home';
 import { Provider } from 'react-redux';
 import { Store } from './src/redux/store';
 
+import { NavigationContainer } from '@react-navigation/native';
+
 const App = () => (
-  <Provider store={Store}>
-    <Home/>
-  </Provider>
+  <NavigationContainer>
+    <Provider store={Store}>
+      <Home/>
+    </Provider>
+  </NavigationContainer>
 )
 
 export default App;
--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,118 +1,13 @@
 import React, {useState, useEffect} from 'react';
-import {View, Text, TouchableOpacity, ScrollView} from 'react-native';
-import { connect } from 'react-redux';
-import { styles } from './style';
-import { RNCamera } from 'react-native-camera';
-import { useCamera } from 'react-native-camera-hooks';
-import Orientation from 'react-native-orientation';
+import {View, Text, TouchableOpacity } from 'react-native';
 
-import {
-  ReqScanner 
-} from '../../redux/actions';
-
-const Home = ({
-  ReqScanner,
-  loadingScanner,
-  dataScan
-}) => {
-  const [viewCamera, setViewCamera] = useState(false);
-  const [{cameraRef, type}] = useCamera();
-  const [scannerBar, setScannerBar] = useState('');
-
-  useEffect(() => {
-    if(!!viewCamera){
-        Orientation.lockToLandscape()
-    }else{
-        Orientation.lockToPortrait()
-    }
-  }, [viewCamera])
-  console.log(dataScan);
-
+const Home = ({}) => {
   return (
-    <>
-      <View style={styles.headerBar}>
-        <Text style={styles.titleText}> Scanner Materiais - GETI </Text>
-      </View>
-      <View style={{flex: 6}}>
-        {viewCamera && (
-          <View style={{flex: 1}}>
-            <RNCamera
-              style={{flex: 1}}
-              ref={cameraRef}
-              type={type}
-              flashMode={RNCamera.Constants.FlashMode.on}
-              androidCameraPermissionOptions={{
-                title: 'Permissão para usar a camera',
-                message:
-                  'Nós precisamos da sua permissão para utilizar sua camera.',
-                buttonPositive: 'Ok',
-                buttonNegative: 'Cancel',
-              }}
-              androidRecordAudioPermissionOptions={{
-                title: 'Permission to use audio recording',
-                message: 'We need your permission to use your audio',
-                buttonPositive: 'Ok',
-                buttonNegative: 'Cancel',
-              }}
-              onBarCodeRead={({data}) => {
-                if (!!data) {
-                  setViewCamera(false);
-                  ReqScanner({codigo_barra: data});
-                }
-              }}>
-              <View>
-                <TouchableOpacity
-                  onPress={() => {
-                    setViewCamera(false);
-                  }}>
-                  <Text style={styles.backButtonScanner}> Voltar </Text>
-                </TouchableOpacity>
-              </View>
-              <View style={styles.contentScan}/> 
-            </RNCamera>
-          </View>
-        )}
-
-        {!viewCamera &&
-          <View style={styles.descriptMaterial}> 
-            <Text> 
-              { !dataScan?.material?.nome_material 
-              ? "Nome do material não encontrado"
-              :`Nome: ${dataScan.material.nome_material}`
-            } </Text>
-             <Text> 
-              { !dataScan?.material?.codigo_barra_material 
-              ? "Código do material não encontrado"
-              : `Código: ${dataScan?.material?.codigo_barra_material}`
-            } </Text>
-          </View>
-        }
-      </View>
-
-      {!viewCamera && (
-        <View style={styles.footerHome}>
-          <TouchableOpacity
-            style={styles.buttonScanner}
-            onPress={() => {
-              setViewCamera(true);
-            }}>
-            <Text style={styles.textSubmit}>Scanner </Text>
-          </TouchableOpacity>
-        </View>
-      )}
-    </>
+    <View> 
+      <Text> Home Page </Text> 
+    </View>
   );
-};
+}
 
-const mapStateToProps = ({ Scanner }) => {
-  const { loadingScanner, dataScan } = Scanner;
+export default Home;
 
-  return {
-    loadingScanner,
-    dataScan
-  }
-} 
-
-export default connect(mapStateToProps,{
-  ReqScanner,
-})(Home);
